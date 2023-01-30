aebc6f0a57facfc81a403c9e67bc0101b3e3300b
Taomercy <35693926+Taomercy@users.noreply.github.com>
2023-01-29 22:16:05 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-build_tag=`git rev-parse --short HEAD').trim()`
+build_tag=`git rev-parse --short HEAD`
 docker build -t taomercy/stock-monitor:${build_tag} .
 docker push taomercy/stock-monitor:${build_tag}
 docker ps -a | grep stock-monitor | awk {'print $1'} | xargs docker stop
